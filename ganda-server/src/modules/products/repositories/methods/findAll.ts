e2a5d6f93e6c94013de72a5b4cd421838e528ce2
DanielBGC <danielbgc02@gmail.com>
2024-10-06 01:55:30 -0300

    fix: Adicionando o campo 'image' no model 'Product' do prisma e retornando apenas os produtos que possuem estoque maior que 0

--- ganda-server/src/modules/products/repositories/methods/findAll.ts
+++ ganda-server/src/modules/products/repositories/methods/findAll.ts
@@ -3,5 +3,11 @@ import { ProductRepository } from '../index';
 import { PrismaEvent } from '../../entities/Product';
 
 export async function findAll(this: ProductRepository): Promise<PrismaEvent[]> {
-  return this.prisma.product.findMany();
+  return this.prisma.product.findMany({
+    where: {
+      stock: {
+        gt: 0, // Retorna apenas produtos com estoque maior que 0
+      },
+    },
+  });
 }
