e575f5b0cd3651cfa9055d16513a97b43b54e9dc
DanielBGC <danielbgc02@gmail.com>
2024-10-07 00:19:18 -0300

    fix: Correção importação

--- ganda-client/src/components/modal.tsx
+++ ganda-client/src/components/modal.tsx
@@ -1,7 +1,7 @@
 import { FiCheckCircle } from 'react-icons/fi';
 import { FaCircleXmark } from 'react-icons/fa6';
 
-import { MainButton } from './MainButton';
+import { MainButton } from './mainButton';
 
 interface ModalProps {
   isOpen: boolean;
