1eb1aec74c71394fe4f43cf917805b69005452ed
DanielBGC <danielbgc02@gmail.com>
2024-10-07 18:51:28 -0300

    Adicionando rota /

--- ganda-server/src/routes/index.ts
+++ ganda-server/src/routes/index.ts
@@ -8,6 +8,10 @@ import { CartRouter } from '../modules/carts/routes/CartRouter';
 export class AppRouter {
   routes = (): Router => {
     const router = Router();
+    router.use('/', (req, res) => {
+      res.status(200).json({ msg: 'Server running!' });
+    });
+
     router.use('/users', UserRouter.routes());
     router.use('/orders', OrderRouter.routes());
     router.use('/products', ProductRouter.routes());
